1c39e531b2d9d12b280ba96554a740ddc1e281db
urmi-21 <mrbains21@gmail.com>
2019-01-08 14:01:00 -0600

    create MOG color themes

---
+++ src/edu/iastate/metnet/metaomgraph/utils/MOGColorThemes.java
@@ -0,0 +1,112 @@
+package edu.iastate.metnet.metaomgraph.utils;
+
+import java.awt.Color;
+
+import javax.swing.plaf.ColorUIResource;
+
+public class MOGColorThemes {
+
+	private  Color tableColor1=null;
+	private  Color tableColor2=null;
+	private  Color tableSelectionColor=null;
+	private  Color tableHighlightColor=null;
+	private  Color tableHyperlinkColor=null;
+	private  Color chartBackgroundColor=null;
+	private  Color plotBackgroundColor=null;
+	
+	MOGColorThemes(Color tableCol1,Color tableCol2, Color tableSelColor, Color tableHlColor, Color tableHlinkColor, Color chartBgColor, Color plotBgColor){
+		this.tableColor1=tableCol1;
+		this.tableColor2=tableCol2;
+		this.tableHighlightColor=tableHlColor;
+		this.tableHyperlinkColor=tableHlColor;
+		this.tableSelectionColor=tableSelColor;
+		this.chartBackgroundColor=chartBgColor;
+		this.plotBackgroundColor=plotBgColor;
+	}
+	
+	public  Color getTableColor1() {
+		if(tableColor1==null) {
+			return Color.white;
+		}else {
+			return tableColor1;
+		}
+	}
+	
+	public  Color getTableColor2() {
+		if(tableColor2==null) {
+			return new ColorUIResource(216, 236, 213);
+		}else {
+			return tableColor2;
+		}
+	}
+	
+	public  Color getTableSelectionColor() {
+		if(tableSelectionColor==null) {
+			return Color.black;
+		}else {
+			return tableSelectionColor;
+		}
+	}
+	
+	public  Color getTableHighlightColor() {
+		if(tableHighlightColor==null) {
+			return Color.PINK;
+		}else {
+			return tableHighlightColor;
+		}
+	}
+	
+	public  Color getTableHyperlinkColor() {
+		if(tableHyperlinkColor==null) {
+			return Color.green;
+		}else {
+			return tableHyperlinkColor;
+		}
+	}
+	
+	public  Color getChartBackgroundColor() {
+		if(chartBackgroundColor==null) {
+			return Color.white;
+		}else {
+			return chartBackgroundColor;
+		}
+	}
+	
+	public  Color getPlotBackgroundColor() {
+		if(plotBackgroundColor==null) {
+			return Color.white;
+		}else {
+			return plotBackgroundColor;
+		}
+	}
+	
+	public  void setTableColor1(Color col) {
+		tableColor1=col;
+	}
+	
+	public  void setTableColor2(Color col) {
+		tableColor2=col;
+	}
+	
+	public  void setTableSelectionColor(Color col) {
+		tableSelectionColor=col;
+	}
+	
+	
+	
+	public  void setTableHighlightColor(Color col) {
+		tableHighlightColor=col;
+	}
+	
+	public  void setTableHyperlinkColor(Color col) {
+		tableHyperlinkColor=col;
+	}
+	
+	public  void setChartBackgroundColor(Color col) {
+		chartBackgroundColor=col;
+	}
+	
+	public  void setPlotBackgroundColor(Color col) {
+		plotBackgroundColor=col;
+	}
+}
