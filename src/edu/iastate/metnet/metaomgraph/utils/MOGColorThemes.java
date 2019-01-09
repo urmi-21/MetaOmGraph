07c86fd16d3ca99b67f79b1fa4da89b923f080b9
urmi-21 <mrbains21@gmail.com>
2019-01-08 21:58:12 -0600

    update theme

--- src/edu/iastate/metnet/metaomgraph/utils/MOGColorThemes.java
+++ src/edu/iastate/metnet/metaomgraph/utils/MOGColorThemes.java
@@ -5,113 +5,121 @@ import java.io.Serializable;
 
 import javax.swing.plaf.ColorUIResource;
 
-public class MOGColorThemes implements Serializable{
+public class MOGColorThemes implements Serializable {
 	private String themeName;
-	private  Color tableColor1=null;
-	private  Color tableColor2=null;
-	private  Color tableSelectionColor=null;
-	private  Color tableHighlightColor=null;
-	private  Color tableHyperlinkColor=null;
-	private  Color chartBackgroundColor=null;
-	private  Color plotBackgroundColor=null;
-	
-	public MOGColorThemes(String name, Color tableCol1,Color tableCol2, Color tableSelColor, Color tableHlColor, Color tableHlinkColor, Color chartBgColor, Color plotBgColor){
-		this.themeName=name;
-		this.tableColor1=tableCol1;
-		this.tableColor2=tableCol2;
-		this.tableHighlightColor=tableHlColor;
-		this.tableHyperlinkColor=tableHlinkColor;
-		this.tableSelectionColor=tableSelColor;
-		this.chartBackgroundColor=chartBgColor;
-		this.plotBackgroundColor=plotBgColor;
+	private Color tableColor1 = null;
+	private Color tableColor2 = null;
+	private Color tableSelectionColor = null;
+	private Color tableHighlightColor = null;
+	private Color tableHyperlinkColor = null;
+	private Color chartBackgroundColor = null;
+	private Color plotBackgroundColor = null;
+
+	public MOGColorThemes() {
+
 	}
-	
+
+	public MOGColorThemes(String name) {
+		this.themeName = name;
+	}
+
+	public MOGColorThemes(String name, Color tableCol1, Color tableCol2, Color tableSelColor, Color tableHlColor,
+			Color tableHlinkColor, Color chartBgColor, Color plotBgColor) {
+		this.themeName = name;
+		this.tableColor1 = tableCol1;
+		this.tableColor2 = tableCol2;
+		this.tableHighlightColor = tableHlColor;
+		this.tableHyperlinkColor = tableHlinkColor;
+		this.tableSelectionColor = tableSelColor;
+		this.chartBackgroundColor = chartBgColor;
+		this.plotBackgroundColor = plotBgColor;
+	}
+
 	public String getThemeName() {
 		return themeName;
 	}
-	public  Color getTableColor1() {
-		if(tableColor1==null) {
+
+	public Color getTableColor1() {
+		if (tableColor1 == null) {
 			return Color.white;
-		}else {
+		} else {
 			return tableColor1;
 		}
 	}
-	
-	public  Color getTableColor2() {
-		if(tableColor2==null) {
+
+	public Color getTableColor2() {
+		if (tableColor2 == null) {
 			return new ColorUIResource(216, 236, 213);
-		}else {
+		} else {
 			return tableColor2;
 		}
 	}
-	
-	public  Color getTableSelectionColor() {
-		if(tableSelectionColor==null) {
+
+	public Color getTableSelectionColor() {
+		if (tableSelectionColor == null) {
 			return Color.black;
-		}else {
+		} else {
 			return tableSelectionColor;
 		}
 	}
-	
-	public  Color getTableHighlightColor() {
-		if(tableHighlightColor==null) {
+
+	public Color getTableHighlightColor() {
+		if (tableHighlightColor == null) {
 			return Color.PINK;
-		}else {
+		} else {
 			return tableHighlightColor;
 		}
 	}
-	
-	public  Color getTableHyperlinkColor() {
-		if(tableHyperlinkColor==null) {
+
+	public Color getTableHyperlinkColor() {
+		if (tableHyperlinkColor == null) {
 			return Color.green;
-		}else {
+		} else {
 			return tableHyperlinkColor;
 		}
 	}
-	
-	public  Color getChartBackgroundColor() {
-		if(chartBackgroundColor==null) {
+
+	public Color getChartBackgroundColor() {
+		if (chartBackgroundColor == null) {
 			return Color.white;
-		}else {
+		} else {
 			return chartBackgroundColor;
 		}
 	}
-	
-	public  Color getPlotBackgroundColor() {
-		if(plotBackgroundColor==null) {
+
+	public Color getPlotBackgroundColor() {
+		if (plotBackgroundColor == null) {
 			return Color.white;
-		}else {
+		} else {
 			return plotBackgroundColor;
 		}
 	}
-	
-	public  void setTableColor1(Color col) {
-		tableColor1=col;
+
+	public void setTableColor1(Color col) {
+		tableColor1 = col;
 	}
-	
-	public  void setTableColor2(Color col) {
-		tableColor2=col;
+
+	public void setTableColor2(Color col) {
+		tableColor2 = col;
 	}
-	
-	public  void setTableSelectionColor(Color col) {
-		tableSelectionColor=col;
+
+	public void setTableSelectionColor(Color col) {
+		tableSelectionColor = col;
 	}
-	
-	
-	
-	public  void setTableHighlightColor(Color col) {
-		tableHighlightColor=col;
+
+	public void setTableHighlightColor(Color col) {
+		tableHighlightColor = col;
 	}
-	
-	public  void setTableHyperlinkColor(Color col) {
-		tableHyperlinkColor=col;
+
+	public void setTableHyperlinkColor(Color col) {
+		tableHyperlinkColor = col;
 	}
-	
-	public  void setChartBackgroundColor(Color col) {
-		chartBackgroundColor=col;
+
+	public void setChartBackgroundColor(Color col) {
+		chartBackgroundColor = col;
 	}
-	
-	public  void setPlotBackgroundColor(Color col) {
-		plotBackgroundColor=col;
+
+	public void setPlotBackgroundColor(Color col) {
+		plotBackgroundColor = col;
 	}
 }
