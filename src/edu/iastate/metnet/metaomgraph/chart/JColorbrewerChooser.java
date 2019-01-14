fb1b9ea87d49f6255d44fc2dea455f8e0fbd2245
urmi-21 <mrbains21@gmail.com>
2019-01-14 00:59:44 -0600

    display palette chooser

--- src/edu/iastate/metnet/metaomgraph/chart/JColorbrewerChooser.java
+++ src/edu/iastate/metnet/metaomgraph/chart/JColorbrewerChooser.java
@@ -5,8 +5,10 @@ import java.awt.EventQueue;
 import java.awt.Paint;
 
 import javax.swing.JInternalFrame;
+import javax.swing.JOptionPane;
 
 import org.jcolorbrewer.ColorBrewer;
+import org.jcolorbrewer.ui.ColorPaletteChooserDialog;
 
 public class JColorbrewerChooser extends JInternalFrame {
 
@@ -32,49 +34,47 @@ public class JColorbrewerChooser extends JInternalFrame {
 	public JColorbrewerChooser() {
 		setBounds(100, 100, 450, 300);
 
+		final ColorPaletteChooserDialog dialog = new ColorPaletteChooserDialog();
+		add(dialog);
 	}
-	
 
 	/**
 	 * get color Palette using jcolorbrewer
+	 * 
 	 * @param n
 	 * @return
 	 */
 	public Paint[] getpaintArray(int n) {
-		
-		
-		/*final ColorPaletteChooserDialog dialog = new ColorPaletteChooserDialog();
-		dialog.show();
-		if(dialog.wasOKPressed()) {
-			Color c = dialog.getColor();
-			String desc=dialog.getColorPalette().getPaletteDescription();
-			JOptionPane.showMessageDialog(null, "desc:"+desc);
-		}*/
-		
-		
+
+		/*
+		 * final ColorPaletteChooserDialog dialog = new ColorPaletteChooserDialog();
+		 * dialog.show(); if(dialog.wasOKPressed()) { Color c = dialog.getColor();
+		 * String desc=dialog.getColorPalette().getPaletteDescription();
+		 * JOptionPane.showMessageDialog(null, "desc:"+desc); }
+		 */
+
 		boolean colorBlindSave = false;
-		//ColorBrewer[] sequentialPalettes = ColorBrewer.getSequentialColorPalettes(colorBlindSave);
+		// ColorBrewer[] sequentialPalettes =
+		// ColorBrewer.getSequentialColorPalettes(colorBlindSave);
 		ColorBrewer[] sequentialPalettes = ColorBrewer.getQualitativeColorPalettes(colorBlindSave);
-		
 
 		ColorBrewer myBrewer = sequentialPalettes[5];
 
-		System.out.println( "Name of this color brewer: " + myBrewer);
+		System.out.println("Name of this color brewer: " + myBrewer);
 
 		// I want a gradient of 8 colors:
 		Color[] myGradient = myBrewer.getColorPalette(5);
 
 		// These are the color codes:
-		for (Color color: myGradient){
+		for (Color color : myGradient) {
 			// convert to hex for web display:
-			String hex = Integer.toHexString(color.getRGB() & 0xffffff);			
-			System.out.println("#"+hex+";");
+			String hex = Integer.toHexString(color.getRGB() & 0xffffff);
+			System.out.println("#" + hex + ";");
 		}
-		
+
 		return myGradient;
-		
-		
-		//return null;
+
+		// return null;
 	}
 
 }
