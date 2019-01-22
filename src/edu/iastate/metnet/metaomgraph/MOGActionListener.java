76df6054fdee019ce5e0ef2ccb21bc8fb8621a24
urmi-21 <mrbains21@gmail.com>
2019-01-22 01:00:48 -0600

    create a common action listener class

---
+++ src/edu/iastate/metnet/metaomgraph/MOGActionListener.java
@@ -0,0 +1,17 @@
+package edu.iastate.metnet.metaomgraph;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MOGActionListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		if ("".equals(e.getActionCommand())) { 
+			
+		}
+	}
+
+}
