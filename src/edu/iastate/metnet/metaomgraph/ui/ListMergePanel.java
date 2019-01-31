2435c1d399680534a9f3b7f1edcaa5579dc0188b
urmi-21 <mrbains21@gmail.com>
2019-01-30 21:05:50 -0600

    list mergeFix

--- src/edu/iastate/metnet/metaomgraph/ui/ListMergePanel.java
+++ src/edu/iastate/metnet/metaomgraph/ui/ListMergePanel.java
@@ -99,7 +99,7 @@ public class ListMergePanel extends JPanel {
 		}
 		
 		//find intersection of all lists in allRows
-		JOptionPane.showMessageDialog(null, allRows.toString());
+		
 		result=Utils.getListIntersection(allRows);
 		return result.toArray(new Integer[0]);
 	}
