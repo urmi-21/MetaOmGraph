8b89cec151a3cc652a3ee9304418c803e2cad7f2
urmi-21 <mrbains21@gmail.com>
2019-02-23 18:05:24 -0600

    def

--- src/edu/iastate/metnet/metaomgraph/ui/DifferentialExpFrame.java
+++ src/edu/iastate/metnet/metaomgraph/ui/DifferentialExpFrame.java
@@ -1,14 +1,32 @@
 package edu.iastate.metnet.metaomgraph.ui;
 
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
 
 import javax.swing.JInternalFrame;
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JLabel;
 import javax.swing.JSplitPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.JList;
+import javax.swing.JButton;
+import javax.swing.AbstractListModel;
+import javax.swing.JComboBox;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.ScrollPaneConstants;
 
 public class DifferentialExpFrame extends JInternalFrame {
+	private JTextField txtGroup1;
+	private JTextField txtGroup2;
+	
+	private JScrollPane jscp1;
+	private JTable tableGrp1;
+	
+	private JScrollPane jscp2;
+	private JTable tableGrp2;
 
 	/**
 	 * Launch the application.
@@ -36,21 +54,98 @@ public class DifferentialExpFrame extends JInternalFrame {
 		JPanel panel = new JPanel();
 		getContentPane().add(panel, BorderLayout.NORTH);
 		
-		JLabel lblTop = new JLabel("Top");
+		JLabel lblTop = new JLabel("Select feature list");
 		panel.add(lblTop);
+		
+		JComboBox comboBox = new JComboBox();
+		panel.add(comboBox);
+		
+		JLabel label = new JLabel("                             ");
+		panel.add(label);
+		
+		JLabel lblSelectMethod = new JLabel("Select method");
+		panel.add(lblSelectMethod);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		panel.add(comboBox_1);
 		
 		JPanel panel_1 = new JPanel();
 		getContentPane().add(panel_1, BorderLayout.SOUTH);
 		
-		JLabel lblBot = new JLabel("Bot");
-		panel_1.add(lblBot);
+		JButton btnOk = new JButton("Ok");
+		panel_1.add(btnOk);
 		
 		JPanel panel_2 = new JPanel();
 		getContentPane().add(panel_2, BorderLayout.CENTER);
 		panel_2.setLayout(new BorderLayout(0, 0));
 		
 		JSplitPane splitPane = new JSplitPane();
+		splitPane.setDividerLocation(0.75D);
 		panel_2.add(splitPane, BorderLayout.CENTER);
+		
+		JPanel panel_3 = new JPanel();
+		splitPane.setRightComponent(panel_3);
+		panel_3.setLayout(new BorderLayout(0, 0));
+		
+		txtGroup2 = new JTextField();
+		txtGroup2.setText("Group2");
+		panel_3.add(txtGroup2, BorderLayout.NORTH);
+		txtGroup2.setColumns(10);
+		
+		JList list2 = new JList();
+		panel_3.add(list2, BorderLayout.CENTER);
+		
+		JButton btnAdd2 = new JButton("Add");
+		JPanel btnPnl2=new JPanel(new FlowLayout());
+		btnPnl2.add(btnAdd2);
+		JButton btnRem2 = new JButton("Remove");
+		btnPnl2.add(btnRem2);
+		JButton btnSearch2 = new JButton("Search");
+		btnPnl2.add(btnSearch2);
+		panel_3.add(btnPnl2, BorderLayout.SOUTH);
+		
+		JPanel panel_4 = new JPanel();
+		splitPane.setLeftComponent(panel_4);
+		panel_4.setLayout(new BorderLayout(0, 0));
+		
+		txtGroup1 = new JTextField();
+		txtGroup1.setText("Group2");
+		panel_4.add(txtGroup1, BorderLayout.NORTH);
+		txtGroup1.setColumns(10);
+		
+		
+		//add table1
+		jscp1= new JScrollPane();
+		panel_4.add(jscp1, BorderLayout.CENTER);
+		tableGrp1=new JTable();
+		tableGrp1.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		tableGrp1.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"dsads", "asd", null, null},
+				{null, null, null, null},
+				{null, "asd", "asd", null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"New column", "New column", "New column", "New column"
+			}
+		));
+		
+		jscp1.setViewportView(tableGrp1);
+		
+		
+		JButton btnAdd1 = new JButton("Add");
+		JPanel btnPnl1=new JPanel(new FlowLayout());
+		btnPnl1.add(btnAdd1);
+		JButton btnRem1 = new JButton("Remove");
+		btnPnl1.add(btnRem1);
+		JButton btnSearch1 = new JButton("Search");
+		btnPnl1.add(btnSearch1);
+		
+		panel_4.add(btnPnl1, BorderLayout.SOUTH);
+		
+		
+		
 
 	}
 
