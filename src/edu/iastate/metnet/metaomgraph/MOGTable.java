d58ccf95584acb67fa6181359f74968a2941d84f
urmi-21 <mrbains21@gmail.com>
2019-02-23 19:40:06 -0600

    def

---
+++ src/edu/iastate/metnet/metaomgraph/MOGTable.java
@@ -0,0 +1,12 @@
+package edu.iastate.metnet.metaomgraph;
+
+import javax.swing.JTable;
+
+/**
+ * Class to create tables for MOG
+ * @author mrbai
+ *
+ */
+public class MOGTable extends JTable {
+
+}
