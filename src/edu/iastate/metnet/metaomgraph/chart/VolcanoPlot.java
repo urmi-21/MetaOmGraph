1d7c8cf1d5bdb5ff9ec19b2497ccdfb294bb2fdd
urmi-21 <mrbains21@gmail.com>
2019-02-25 17:41:10 -0600

    volcano plots

--- src/edu/iastate/metnet/metaomgraph/chart/VolcanoPlot.java
+++ src/edu/iastate/metnet/metaomgraph/chart/VolcanoPlot.java
@@ -2,33 +2,64 @@ package edu.iastate.metnet.metaomgraph.chart;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Paint;
+import java.awt.Point;
+import java.awt.Shape;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFormattedTextField;
 import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.text.DefaultFormatterFactory;
 
+import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartMouseEvent;
 import org.jfree.chart.ChartMouseListener;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.entity.ChartEntity;
+import org.jfree.chart.entity.XYItemEntity;
+import org.jfree.chart.plot.DefaultDrawingSupplier;
+import org.jfree.chart.plot.DrawingSupplier;
+import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYItemRenderer;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.util.ShapeUtilities;
 
+import edu.iastate.metnet.metaomgraph.ComponentToImage;
+import edu.iastate.metnet.metaomgraph.GraphFileFilter;
 import edu.iastate.metnet.metaomgraph.IconTheme;
 import edu.iastate.metnet.metaomgraph.MetaOmGraph;
 import edu.iastate.metnet.metaomgraph.MetaOmProject;
+import edu.iastate.metnet.metaomgraph.utils.Utils;
 
 public class VolcanoPlot extends JInternalFrame implements ChartMouseListener, ActionListener {
 
@@ -194,8 +225,203 @@ public class VolcanoPlot extends JInternalFrame implements ChartMouseListener, A
 
 		// Create dataset
 		dataset = createVolcanoDataset();
+		myChart = ChartFactory.createScatterPlot("", "log2 fold change", "-log10 p-value", dataset);
 
-		return null;
+		// Changes background color
+		Shape shape = ShapeUtilities.createRegularCross(2, 1);
+		XYPlot plot = (XYPlot) myChart.getPlot();
+		plot.setBackgroundPaint(plotbg);
+		myChart.setBackgroundPaint(chartbg);
+		// plot.setpaint
+		// XYItemRenderer renderer = plot.getRenderer();
+		myRenderer = plot.getRenderer();
+
+		// use palette if available
+		if (colorArray != null) {
+			plot.setDrawingSupplier((DrawingSupplier) new DefaultDrawingSupplier(colorArray,
+					DefaultDrawingSupplier.DEFAULT_FILL_PAINT_SEQUENCE,
+					DefaultDrawingSupplier.DEFAULT_OUTLINE_PAINT_SEQUENCE,
+					DefaultDrawingSupplier.DEFAULT_STROKE_SEQUENCE,
+					DefaultDrawingSupplier.DEFAULT_OUTLINE_STROKE_SEQUENCE,
+					DefaultDrawingSupplier.DEFAULT_SHAPE_SEQUENCE));
+		} else {
+			Paint[] defaultPaint = DefaultDrawingSupplier.DEFAULT_PAINT_SEQUENCE;
+			Color[] defaultColor = Utils.paintArraytoColor(defaultPaint);
+			plot.setDrawingSupplier((DrawingSupplier) new DefaultDrawingSupplier(Utils.filterColors(defaultColor),
+					DefaultDrawingSupplier.DEFAULT_FILL_PAINT_SEQUENCE,
+					DefaultDrawingSupplier.DEFAULT_OUTLINE_PAINT_SEQUENCE,
+					DefaultDrawingSupplier.DEFAULT_STROKE_SEQUENCE,
+					DefaultDrawingSupplier.DEFAULT_OUTLINE_STROKE_SEQUENCE,
+					DefaultDrawingSupplier.DEFAULT_SHAPE_SEQUENCE));
+		}
+		// Create Panel
+		// use full constructor otherwise tooltips dont work
+		ChartPanel chartPanel = new ChartPanel(myChart, 800, 600, 2, 2, 10000, 10000, true, true, true, true, true,
+				true) {
+			private Dimension oldSize = new Dimension(100, 100);
+
+			@Override
+			public void paintComponent(Graphics g) {
+				if (!oldSize.equals(getSize())) {
+					oldSize = getSize();
+					if (myChart.getBackgroundPaint() instanceof GradientPaint) {
+						GradientPaint gp = (GradientPaint) myChart.getBackgroundPaint();
+						myChart.setBackgroundPaint(Color.WHITE);
+					}
+				}
+
+				super.paintComponent(g);
+				// Custom drawing
+				Graphics2D g2d = (Graphics2D) g.create();
+				Color bg = (Color) myChart.getPlot().getBackgroundPaint();
+				g2d.setColor(new Color(Color.WHITE.getRGB() - bg.getRGB()));
+				g2d.dispose();
+			}
+
+			public void actionPerformed(ActionEvent e) {
+				if (e.getActionCommand().equals(ChartPanel.SAVE_COMMAND)) {
+					File destination = null;
+					JFileChooser chooseDialog = new JFileChooser(Utils.getLastDir());
+					chooseDialog.setFileSelectionMode(JFileChooser.FILES_ONLY);
+					chooseDialog.setFileFilter(new GraphFileFilter(GraphFileFilter.PNG));
+					JPanel sizer = new JPanel();
+					JFormattedTextField widthField, heightField;
+					JFormattedTextField.AbstractFormatter af = new JFormattedTextField.AbstractFormatter() {
+
+						public Object stringToValue(String text) throws ParseException {
+							try {
+								return new Integer(text);
+							} catch (NumberFormatException nfe) {
+								return getFormattedTextField().getValue();
+							}
+						}
+
+						public String valueToString(Object value) throws ParseException {
+							if (value instanceof Integer) {
+								Integer intValue = (Integer) value;
+								if (intValue.intValue() < 1)
+									return "1";
+								else
+									return ((Integer) value).intValue() + "";
+							}
+							return null;
+						}
+
+					};
+					widthField = new JFormattedTextField(new DefaultFormatterFactory(af), new Integer(getWidth()));
+					heightField = new JFormattedTextField(new DefaultFormatterFactory(af), new Integer(getHeight()));
+					widthField.setColumns(4);
+					heightField.setColumns(4);
+					sizer.setLayout(new GridBagLayout());
+					GridBagConstraints c = new GridBagConstraints();
+					c.gridwidth = 3;
+					c.fill = GridBagConstraints.NONE;
+					sizer.add(new JLabel("Image size:"), c);
+					c.gridwidth = 1;
+					c.gridy = 1;
+					sizer.add(new JLabel("Width:"), c);
+					c.gridx = 1;
+					sizer.add(widthField, c);
+					c.gridx = 2;
+					sizer.add(new JLabel("pixels"), c);
+					c.gridx = 0;
+					c.gridy = 2;
+					sizer.add(new JLabel("Height:"), c);
+					c.gridx = 1;
+					sizer.add(heightField, c);
+					c.gridx = 2;
+					sizer.add(new JLabel("pixels"), c);
+					chooseDialog.setAccessory(sizer);
+					int returnVal = JFileChooser.APPROVE_OPTION;
+					/*
+					 * Continually show a file chooser until user selects a valid location, or
+					 * cancels.
+					 */
+					boolean ready = false;
+					while (!ready) {
+						while (((destination == null)) && (returnVal != JFileChooser.CANCEL_OPTION)) {
+							returnVal = chooseDialog.showSaveDialog(MetaOmGraph.getMainWindow());
+							destination = chooseDialog.getSelectedFile();
+						}
+						// Did user cancel? If so, don't do anything.
+						if (returnVal == JFileChooser.CANCEL_OPTION)
+							return;
+						// Check if file exists, prompt to overwrite if it
+						// does
+						String filename = destination.getAbsolutePath();
+						if (!filename.substring(filename.length() - 4).equals(".png")) {
+							filename += ".png";
+							destination = new File(filename);
+						}
+						if (destination.exists()) {
+							int overwrite = JOptionPane.showConfirmDialog(MetaOmGraph.getMainWindow(),
+									filename + " already exists.  Overwrite?", "Overwrite File",
+									JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE);
+							if ((overwrite == JOptionPane.CANCEL_OPTION) || (overwrite == JOptionPane.CLOSED_OPTION))
+								return;
+							else if (overwrite == JOptionPane.YES_OPTION)
+								ready = true;
+							else
+								destination = null; // No option
+						} else
+							ready = true;
+					}
+					final int oldDrawWidth = getMaximumDrawWidth();
+					final int oldDrawHeight = getMaximumDrawHeight();
+					final int newWidth = Integer.parseInt(widthField.getText());
+					final int newHeight = Integer.parseInt(heightField.getText());
+					final File trueDest = new File(destination.getAbsolutePath());
+					setMaximumDrawWidth(newWidth);
+					setMaximumDrawHeight(newHeight);
+					try {
+						ComponentToImage.saveAsPNG(this, trueDest, newWidth, newHeight);
+					} catch (IOException ioe) {
+						ioe.printStackTrace();
+					}
+					setMaximumDrawWidth(oldDrawWidth);
+					setMaximumDrawHeight(oldDrawHeight);
+				} else
+					super.actionPerformed(e);
+			}
+
+			@Override
+			public String getToolTipText(MouseEvent event) {
+				return "tooooool";
+			}
+
+			// urmi display tooltip away from point
+			@Override
+			public Point getToolTipLocation(MouseEvent event) {
+				Point thisPoint = event.getPoint();
+				Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+				// int maxWidth=(int) screenSize.getWidth();
+				int maxWidth = getWidth();
+				// define horizontal space between tooltip and point
+				int xMargin = 25;
+
+				int y = thisPoint.y;
+				int newy = 100;
+				/*
+				 * select appropriate y if(y-200<=0) { newy=10; }else { newy=y-200; }
+				 */
+				int x = thisPoint.x;
+				// JOptionPane.showMessageDialog(null, "mw:"+maxWidth+" x:"+x);
+				// if point is far right of scree show tool tip to the left
+				if (maxWidth - x <= 450) {
+					// JOptionPane.showMessageDialog(null, "mw:"+maxWidth+" x:"+x);
+					// return new Point(x-300, 5);
+					// table width is 400
+					return new Point(x - (400 + xMargin), newy);
+				}
+				return new Point(x + xMargin, newy);
+			}
+
+		};
+
+		chartPanel.addChartMouseListener(this);
+
+
+		return chartPanel;
 	}
 	
 	private XYDataset createVolcanoDataset() throws IOException {
@@ -204,7 +430,7 @@ public class VolcanoPlot extends JInternalFrame implements ChartMouseListener, A
 		XYSeries series1 = new XYSeries(xAxisname + " vs. ");
 		for(int i=0;i<foldChange.size();i++) {
 			double thisFC=foldChange.get(i);
-			double thisPV=(-1*Math.log10(pVals.get(i)));
+			double thisPV=-1*Math.log10(pVals.get(i));
 			series1.add(thisFC, thisPV);
 		}
 		
