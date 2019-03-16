1f94fc78b850afca816dd936100999f8656f085e
urmi-21 <mrbains21@gmail.com>
2019-03-15 19:18:14 -0500

    version

--- src/edu/iastate/metnet/metaomgraph/MetaOmGraph.java
+++ src/edu/iastate/metnet/metaomgraph/MetaOmGraph.java
@@ -413,8 +413,8 @@ public class MetaOmGraph implements ActionListener {
 	}
 
 	/////////////////////
-	private static final String VERSION = "1.7.5";
-	private static final String DATE = "Feb 4, 2019";
+	private static final String VERSION = "1.7.6Beta";
+	private static final String DATE = "March 15, 2019";
 
 	public static String getVersion() {
 		return VERSION;
