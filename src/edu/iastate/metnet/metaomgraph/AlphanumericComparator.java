4e7289e2c7b312158126742758523ddf8da633ac
urmi-21 <mrbains21@gmail.com>
2019-03-19 02:06:26 -0500

    fixed case sensitive comparator

--- src/edu/iastate/metnet/metaomgraph/AlphanumericComparator.java
+++ src/edu/iastate/metnet/metaomgraph/AlphanumericComparator.java
@@ -16,8 +16,9 @@ public class AlphanumericComparator implements Comparator {
 	}
 	
 	public int compare(Object o1, Object o2) {
-		String s1 = o1.toString();
-		String s2 = o2.toString();
+		//convert to string and lower case for case insensitive match
+		String s1 = o1.toString().toLowerCase();
+		String s2 = o2.toString().toLowerCase();
 		return compareNatural(s1, s2);
 	}
 
