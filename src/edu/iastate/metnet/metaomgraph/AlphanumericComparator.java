543f34d95de5f31be628f77cc35c01795ebfddfd
urmi-21 <mrbains21@gmail.com>
2019-03-19 00:38:06 -0500

    better alphanumeric comparator

--- src/edu/iastate/metnet/metaomgraph/AlphanumericComparator.java
+++ src/edu/iastate/metnet/metaomgraph/AlphanumericComparator.java
@@ -14,8 +14,15 @@ import javax.swing.JOptionPane;
 public class AlphanumericComparator implements Comparator {
 	public AlphanumericComparator() {
 	}
-
+	
 	public int compare(Object o1, Object o2) {
+		String s1 = o1.toString();
+		String s2 = o2.toString();
+		return compareNatural(s1, s2);
+	}
+
+	//this approach doesn't always work; results in java.lang.IllegalArgumentException: Comparison method violates its general contract! exception
+	public int compareOLD(Object o1, Object o2) {
 		String s1 = o1.toString();
 		String s2 = o2.toString();
 		final Double num1 = getDouble(s1);
@@ -43,5 +50,46 @@ public class AlphanumericComparator implements Comparator {
 		} catch (NumberFormatException e) {
 			return null;
 		}
+	}
+	
+	//function for comparing alphanumeric strings
+	//code reference https://stackoverflow.com/questions/7270447/java-string-number-comparator
+	//could be improved later
+	public  int compareNatural(String a, String b) {
+	    int la = a.length();
+	    int lb = b.length();
+	    int ka = 0;
+	    int kb = 0;
+	    while (true) {
+	        if (ka == la)
+	            return kb == lb ? 0 : -1;
+	        if (kb == lb)
+	            return 1;
+	        if (a.charAt(ka) >= '0' && a.charAt(ka) <= '9' && b.charAt(kb) >= '0' && b.charAt(kb) <= '9') {
+	            int na = 0;
+	            int nb = 0;
+	            while (ka < la && a.charAt(ka) == '0')
+	                ka++;
+	            while (ka + na < la && a.charAt(ka + na) >= '0' && a.charAt(ka + na) <= '9')
+	                na++;
+	            while (kb < lb && b.charAt(kb) == '0')
+	                kb++;
+	            while (kb + nb < lb && b.charAt(kb + nb) >= '0' && b.charAt(kb + nb) <= '9')
+	                nb++;
+	            if (na > nb)
+	                return 1;
+	            if (nb > na)
+	                return -1;
+	            if (ka == la)
+	                return kb == lb ? 0 : -1;
+	            if (kb == lb)
+	                return 1;
+
+	        }
+	        if (a.charAt(ka) != b.charAt(kb))
+	            return a.charAt(ka) - b.charAt(kb);
+	        ka++;
+	        kb++;
+	    }
 	}
 }
