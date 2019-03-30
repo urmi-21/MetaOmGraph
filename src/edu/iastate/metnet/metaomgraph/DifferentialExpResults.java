99aa64f11efaf46c3724e64cba63ab731d6eef42
urmi-21 <mrbains21@gmail.com>
2019-03-30 16:05:21 -0500

    diff exp class

--- src/edu/iastate/metnet/metaomgraph/DifferentialExpResults.java
+++ src/edu/iastate/metnet/metaomgraph/DifferentialExpResults.java
@@ -49,58 +49,79 @@ public class DifferentialExpResults {
 		this.pval = pval;
 
 	}
-	
-	
-	//methods to retrieve data
+
+	// methods to retrieve data
 	public String getmethodName() {
 		// 1 MW U test; 2 t test; 3 Weltch t test; 4 Paired t test; 5 Wilcoxon signed
-		if(method==1) {
+		if (method == 1) {
 			return "M-W U test";
 		}
-		if(method==2) {
+		if (method == 2) {
 			return "Student's t test";
 		}
-		if(method==3) {
+		if (method == 3) {
 			return "Weltch's t test";
 		}
-		if(method==4) {
+		if (method == 4) {
 			return "Paired t test";
 		}
-		if(method==5) {
+		if (method == 5) {
 			return "Wilcoxon signed-rank test";
 		}
-		
-		
+
 		return "unknown";
 	}
-	
+
 	public String getGrp1Name() {
 		return grp1;
 	}
-	
+
 	public String getGrp2Name() {
 		return grp2;
 	}
-	
+
 	public String getGeneListName() {
 		return geneListName;
 	}
-	
+
 	public String geDataTransform() {
 		return dataTransform;
 	}
-	
+
 	public int getGrp1Size() {
 		return grp1Size;
 	}
-	
+
 	public int getGrp2Size() {
 		return grp2Size;
 	}
-	
-	
-	
-	
-	
+
+	public List<String> getRowNames() {
+		return rowNames;
+	}
+
+	public List<Double> getMeanGrp1() {
+		return meanGrp1;
+	}
+
+	public List<Double> getMeanGrp2() {
+		return meanGrp2;
+	}
+
+	public List<Double> getlogFC() {
+		return logFC;
+	}
+
+	public List<Double> getfStat() {
+		return fStat;
+	}
+
+	public List<Double> getFPVal() {
+		return fPval;
+	}
+
+	public List<Double> getPVal() {
+		return pval;
+	}
 
 }
