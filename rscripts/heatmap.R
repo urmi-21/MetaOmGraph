1c5248083a64e9e06ee2aa539d598990e2c5b78b
urmi-21 <mrbains21@gmail.com>
2019-04-22 16:11:22 -0500

    update R interface

---
+++ rscripts/heatmap.R
@@ -0,0 +1,44 @@
+#!/usr/bin/env Rscript
+
+#author: urmi
+#script to create heatmap
+
+library(dplyr)
+library(plyr)
+library(scales)
+library(readr)
+library(data.table)
+library(ggplot2)
+
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)<2) {
+  stop("Invalid Arguments", call.=FALSE)
+} 
+
+#read data
+infile<-args[1]
+metadataFile<-args[2]
+outDir<-args[3]
+
+data<-read_delim(infile,"\t", escape_double = FALSE, trim_ws = TRUE)
+rownames(data)<-data$Name
+data$Name<-NULL
+tdata<-as.data.frame(t(data))
+tdata$Name<-rownames(tdata)
+data.melt<-melt(tdata)
+data.melt <- ddply(data.melt, .(variable), transform, rescale = rescale(value))
+data.melt$Name <- factor(data.melt$Name, levels=unique(as.character(data.melt$Name)) )
+data.melt<-data.melt[order(data.melt$Name),]
+
+
+plotHeight<-ncol(data)/10
+pdf(paste(outDir,.Platform$file.sep,"heatmap.pdf",sep = ""),height = plotHeight,width = 10)
+
+ggplot(data.melt, aes(variable, Name)) + geom_tile(aes(fill = rescale),colour = "white") + 
+  scale_fill_gradient(low = "white", high = "red")+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(), axis.line = element_line(colour = "black"))
+  
+dev.off()
+
