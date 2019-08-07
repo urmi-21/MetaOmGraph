a5a86dd455c9e2fa38f38a5532c985958a36635b
urmi-21 <mrbains21@gmail.com>
2019-08-07 15:28:23 -0500

    add holms method

--- src/edu/iastate/metnet/metaomgraph/AdjustPval.java
+++ src/edu/iastate/metnet/metaomgraph/AdjustPval.java
@@ -44,16 +44,15 @@ public class AdjustPval {
 	public double[] getBHAdj(double[] pvals) {
 		int m = pvals.length;
 		double[] res = new double[m];
-		
+
 		// idx now contains indices in sorted order
-		Integer[] idx = getSortedIndex(pvals);
+		int[] idx = getSortedIndex(pvals);
 		// get sorted pvalues
 		double[] pvSorted = new double[m];
 		for (int i = 0; i < idx.length; i++) {
 			pvSorted[i] = pvals[idx[i]];
 		}
-		
-		
+
 		// start correction
 		for (int i = m - 1; i >= 0; i--) {
 			if (i == m - 1) {
@@ -79,23 +78,80 @@ public class AdjustPval {
 		}
 		return resSorted;
 	}
+	
+	
 
 	public double[] getHolmsAdj(double[] pvals) {
 		int m = pvals.length;
-		double[] res = new double[m];
-
-		// idx now contains indices in sorted order
-		Integer[] idx = getSortedIndex(pvals);
-		// get sorted pvalues
-		double[] pvSorted = new double[m];
-		for (int i = 0; i < idx.length; i++) {
-			pvSorted[i] = pvals[idx[i]];
+		int[] sortedInd = getSortedIndex(pvals);
+		double[] sortedIndDouble = intToDouble(sortedInd);
+		double[] cummaxInput = new double[m];
+		for (int i = 0; i < m; ++i) {
+			cummaxInput[i] = (m - i) * pvals[sortedInd[i]];
 		}
-		
-		
-		
+		int[] ro = getSortedIndex(sortedIndDouble);
+		double[] cummaxOutput = cummax(cummaxInput);
+		double[] pmin = pminx(cummaxOutput, 1.0);
+		double[] result = new double[m];
+		for (int i = 0; i < m; ++i) {
+			result[i] = pmin[ro[i]];
+		}
+		return result;
 
-		return res;
+	}
+
+	/*private static int[] order(double[] array, boolean decreasing) {
+		int size = array.length;
+		int[] idx = new int[size];
+		double[] baseArr = new double[size];
+		for (int i = 0; i < size; ++i) {
+			baseArr[i] = array[i];
+			idx[i] = i;
+		}
+
+		Comparator<Integer> cmp;
+		if (!decreasing) {
+			cmp = Comparator.comparingDouble(a -> baseArr[a]);
+		} else {
+			cmp = (a, b) -> Double.compare(baseArr[b], baseArr[a]);
+		}
+
+		return Arrays.stream(idx).boxed().sorted(cmp).mapToInt(a -> a).toArray();
+	}*/
+
+	private static double[] intToDouble(int[] array) {
+		double[] result = new double[array.length];
+		for (int i = 0; i < array.length; i++) {
+			result[i] = array[i];
+		}
+		return result;
+	}
+
+	private static double[] pminx(double[] array, double x) {
+		if (array.length < 1)
+			throw new IllegalArgumentException("pmin requires at least one element");
+		double[] result = new double[array.length];
+		for (int i = 0; i < array.length; ++i) {
+			if (array[i] < x) {
+				result[i] = array[i];
+			} else {
+				result[i] = x;
+			}
+		}
+		return result;
+	}
+
+	private static double[] cummax(double[] array) {
+		if (array.length < 1)
+			throw new IllegalArgumentException("cummax requires at least one element");
+		double[] output = new double[array.length];
+		double cumulativeMax = array[0];
+		for (int i = 0; i < array.length; ++i) {
+			if (array[i] > cumulativeMax)
+				cumulativeMax = array[i];
+			output[i] = cumulativeMax;
+		}
+		return output;
 	}
 
 	/**
@@ -104,7 +160,7 @@ public class AdjustPval {
 	 * @param pvals
 	 * @return
 	 */
-	Integer[] getSortedIndex(double[] pvals) {
+	int[] getSortedIndex(double[] pvals) {
 		int m = pvals.length;
 		// get sorted order of pvals
 		Integer[] idx = new Integer[m];
@@ -115,8 +171,12 @@ public class AdjustPval {
 				return Double.compare(pvals[i1], pvals[i2]);
 			}
 		});
-
-		return idx;
+		
+		int[] res=new int[idx.length];
+		for(int i=0;i<idx.length;i++) {
+			res[i]=idx[i];
+		}
+		return res;
 	}
 
 	/**
@@ -136,8 +196,9 @@ public class AdjustPval {
 		AdjustPval ob = new AdjustPval();
 		double[] pv = { 1, 0, 0.1, 0.3, 0.005, 0.025 };
 		System.out.println("Orig: " + Arrays.toString((pv)));
-		System.out.println("res: " + Arrays.toString(ob.getBHAdj(pv)));
-		System.out.println("res: " + Arrays.toString(ob.getBonferroniAdj(pv)));
+		System.out.println("res BH: " + Arrays.toString(ob.getBHAdj(pv)));
+		System.out.println("res bonf: " + Arrays.toString(ob.getBonferroniAdj(pv)));
+		System.out.println("res holms: " + Arrays.toString(ob.getHolmsAdj(pv)));
 	}
 
 }
