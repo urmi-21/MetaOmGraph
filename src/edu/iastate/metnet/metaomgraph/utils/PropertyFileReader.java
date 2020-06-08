9b4bf4b5a568e11ea3f7c93ca909cf4f8f0399d1
sumanth <sumanth@iastate.edu>
2020-06-08 13:49:25 -0500

    Added comments to property file reader.

--- src/edu/iastate/metnet/metaomgraph/utils/PropertyFileReader.java
+++ src/edu/iastate/metnet/metaomgraph/utils/PropertyFileReader.java
@@ -10,11 +10,16 @@ import java.util.Properties;
 
 /**
  * @author sumanth
- *
+ * Read files with .properties extensions
  */
 public class PropertyFileReader {
 	private Properties prop;
 	
+	/**
+	 * Constructor
+	 * @param propFileName
+	 * @throws IOException
+	 */
 	public PropertyFileReader(String propFileName) throws IOException {
 		prop = new Properties();
 		InputStream inputStream = null;
@@ -27,6 +32,11 @@ public class PropertyFileReader {
 			throw new FileNotFoundException("Cannot find properties file");
 	}
 	
+	/**
+	 * Get the vale of a property
+	 * @param name propertyName
+	 * @return propertyValue
+	 */
 	public String getProperty(String name) {
 		return prop.getProperty(name, "");
 	}
