1fe0353585e0213fdb8309cda1d83835e5c06672
Kumara Sri Harsha Vajjhala <harshavk@Kumaras-MacBook-Air.local>
2021-11-08 14:28:10 -0600

    PCA validation

--- src/edu/iastate/metnet/metaomgraph/ComputePCA.java
+++ src/edu/iastate/metnet/metaomgraph/ComputePCA.java
@@ -124,7 +124,7 @@ public class ComputePCA {
 
 
 			for(int i=0; i<rows; i++) {
-				for(int j=0; j<cols; j++) {
+				for(int j=0; j<2; j++) {
 					System.out.print(output[i][j]+ " ");
 					sb.append(output[i][j]);
 					if(j != cols-1)
