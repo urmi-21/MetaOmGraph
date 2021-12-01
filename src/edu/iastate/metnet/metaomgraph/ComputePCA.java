bb9d0ad206c9024a21443191f10bcd6f7acf2bfe
Kumara Sri Harsha Vajjhala <harshavk@sriharshavk.student.iastate.edu>
2021-11-30 20:16:31 -0600

    Changes to json parsing

--- src/edu/iastate/metnet/metaomgraph/ComputePCA.java
+++ src/edu/iastate/metnet/metaomgraph/ComputePCA.java
@@ -12,6 +12,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import javax.script.*;
+import org.renjin.script.*;
+
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dimensionalityreduction.PCA;
 import org.nd4j.linalg.factory.Nd4j;
@@ -92,59 +95,71 @@ public class ComputePCA {
 
 	public static void main(String [] args) {
 
-		List<List<Double>> records = new ArrayList<>();
-		try (Scanner scanner = new Scanner(new File("/Users/harshavk/Documents/Harsha/Fall2021/Thesis/Human-Cancer-Prediction/X.csv"));) {
-			while (scanner.hasNextLine()) {
-				records.add(getRecordFromLine(scanner.nextLine()));
-			}
-		} catch (FileNotFoundException e) {
+//		List<List<Double>> records = new ArrayList<>();
+//		try (Scanner scanner = new Scanner(new File("/Users/harshavk/Documents/Harsha/Fall2021/Thesis/Human-Cancer-Prediction/X.csv"));) {
+//			while (scanner.hasNextLine()) {
+//				records.add(getRecordFromLine(scanner.nextLine()));
+//			}
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//
+//		int rows = records.size();
+//		int cols = records.get(0).size();
+//
+//		double[][] data = new double[rows][cols];
+//
+//		for(int i=0; i<rows; i++) {
+//			for(int j=0; j<cols; j++) {
+//				data[i][j] = records.get(i).get(j);
+//			}
+//		}
+//
+//		System.out.println("rows : "+rows +" , cols: "+cols);
+//		ComputePCA cp = new ComputePCA(data);
+//
+//		double [][] output = cp.projectData(2, true);
+//
+//		try {
+//			BufferedWriter br = new BufferedWriter(new FileWriter("/Users/harshavk/Documents/Harsha/Fall2021/Thesis/Human-Cancer-Prediction/pca-op.csv"));
+//			StringBuilder sb = new StringBuilder();
+//
+//
+//			for(int i=0; i<rows; i++) {
+//				for(int j=0; j<2; j++) {
+//					System.out.print(output[i][j]+ " ");
+//					sb.append(output[i][j]);
+//					if(j != cols-1)
+//						sb.append(",");
+//				}
+//				sb.append("\n");
+//				System.out.println();
+//			}
+//
+//
+//
+//			br.write(sb.toString());
+//			br.close();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+
+
+		RenjinScriptEngineFactory factory = new RenjinScriptEngineFactory();
+	    // create a Renjin engine:
+	    ScriptEngine engine = factory.getScriptEngine();
+
+	    try {
+			engine.eval("df <- data.frame(x=1:10, y=(1:10)+rnorm(n=10))");
+			engine.eval("print(df)");
+			engine.eval("print(lm(y ~ x, df))");
+		} catch (ScriptException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		int rows = records.size();
-		int cols = records.get(0).size();
-
-		double[][] data = new double[rows][cols];
-
-		for(int i=0; i<rows; i++) {
-			for(int j=0; j<cols; j++) {
-				data[i][j] = records.get(i).get(j);
-			}
-		}
-
-		System.out.println("rows : "+rows +" , cols: "+cols);
-		ComputePCA cp = new ComputePCA(data);
-
-		double [][] output = cp.projectData(2, true);
-
-		try {
-			BufferedWriter br = new BufferedWriter(new FileWriter("/Users/harshavk/Documents/Harsha/Fall2021/Thesis/Human-Cancer-Prediction/pca-output.csv"));
-			StringBuilder sb = new StringBuilder();
-
-
-			for(int i=0; i<rows; i++) {
-				for(int j=0; j<2; j++) {
-					System.out.print(output[i][j]+ " ");
-					sb.append(output[i][j]);
-					if(j != cols-1)
-						sb.append(",");
-				}
-				sb.append("\n");
-				System.out.println();
-			}
-
-
-
-			br.write(sb.toString());
-			br.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-
-
+	   
 
 	}
 }
